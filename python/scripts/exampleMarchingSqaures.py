b54ff2e35f85a3168a81b0599e6ff3379bd40a67
Muhammad Hamza Sattar <mhamza.sattar@tum.de>
2020-04-27 01:18:26 +0200

    Added some more functions to visualize

--- python/scripts/exampleMarchingSqaures.py
+++ python/scripts/exampleMarchingSqaures.py
@@ -4,12 +4,15 @@ from math import cos, sin, exp
 from numpy import linspace
 
 
-def circle(x,y):
+def func(x,y):
     # return sin(5 * x) * cos(5 * y)/5
-    return sin(0.1 * x * x + 0.2 * y * y)
+    # return sin(0.1 * x * x + 0.2 * y * y)
     # return (1-(x**2+y**3))*exp(-(x**2+y**2)/2)
+    # return exp(sin(x) + cos(y)) -sin(exp(x+y))
+    # return sin(sin(x) + cos(y)) - cos(sin(x*y) + cos(x))
+    return sin(x**2 + y**2) - cos(x*y)
 
-ms = MarchingSquares(circle, [-10, 10], [-10,10], [250,250])
+ms = MarchingSquares(func, [-10, 10], [-10,10], [250,250])
 
 # levels = linspace(0,1,11)
 
